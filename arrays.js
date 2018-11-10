cff933decc3b09c0fb35c4a5c2cbd69b5111781a
ali.mohammed200 <ali.mohammed200@gmail.com>
2018-11-10 10:14:19 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,10 @@
 let chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+function addElementToBeginningOfArray(arr, elem){
+  let newArr = arr.slice();
+  newArr.unshift(elem);
+  return newArr;
+}
+function destructivelyAddElementToBeginningOfArray(arr, elem){
+  arr.unshift(elem);
+  return arr;
+}
